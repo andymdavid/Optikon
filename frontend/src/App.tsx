7afdf27747acd3d55f79f90f69c13f0c869ba20c
andymdavid <andy@otherstuff.studio>
2026-01-19 20:07:47 +0800

    Opt into React Router v7 flags

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -38,7 +38,9 @@ function App() {
   }, [])
 
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
       <Routes>
         <Route path="/" element={<BoardsHome apiBaseUrl={API_BASE_URL} />} />
         <Route
